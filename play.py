efd10f129d7383fc3a7d13cc2226203ed69f00ef
HappyandEric <88917824+HappyandEric@users.noreply.github.com>
2021-09-04 18:13:32 +0800

    Update play.py

--- play.py
+++ play.py
@@ -1,25 +1,2 @@
-# coding:utf-8
-import random
-
-a = random.randint(1, 20)
-isok = False
-i=0
-while isok == False:
-    print(a)
-    b=int(input("數字>"))
-
-    if a == b:
-        print("good")
-        break
-    elif i == 4:
-        print("again")
-        break
-    elif b>a:
-        print("小一點")
-        i=i+1
-    elif b<a:
-        print("大一點")
-        i=i+1
-
-    else:
-        isok = True
+for a in[10,9,8,7,6,5,4,3,2,1]:
+    print("!"*a)
