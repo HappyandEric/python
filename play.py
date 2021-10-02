f153f0e3ae991259f05cb183c548eb543320c85f
HappyandEric <88917824+HappyandEric@users.noreply.github.com>
2021-10-02 09:56:47 +0800

    Update play.py

--- play.py
+++ play.py
@@ -1,2 +1,19 @@
-for a in[10,9,8,7,6,5,4,3,2,1]:
-    print("!"*a)
+total=0
+a=0
+h=0
+l=100
+scores=[]
+s=int(input("student?>"))
+for i in range(s):
+    score=int(input("score?>"))
+    scores.append(score)
+for i in range(s):
+    total=scores[i]+total
+    if scores[i]>h:
+        h=scores[i]
+    if scores[i]<l:
+        l=scores[i]
+a=total/s
+print(a)
+print("hightest:",h)
+print("losest:",l)
