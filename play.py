b3e1b8f20972f94c746ec63c45f9395213050125
HappyandEric <88917824+HappyandEric@users.noreply.github.com>
2021-10-09 17:56:26 +0800

    Update play.py

--- play.py
+++ play.py
@@ -1,19 +1,26 @@
-total=0
-a=0
-h=0
-l=100
-scores=[]
-s=int(input("student?>"))
-for i in range(s):
-    score=int(input("score?>"))
-    scores.append(score)
-for i in range(s):
-    total=scores[i]+total
-    if scores[i]>h:
-        h=scores[i]
-    if scores[i]<l:
-        l=scores[i]
-a=total/s
-print(a)
-print("hightest:",h)
-print("losest:",l)
+a=["吐司,50"]
+b=["布丁,15"]
+c=["apple,40"]
+d=["ice cream,65"]
+print("""1號餐:吐司,40
+2號餐:布丁,15
+3號餐:apple,40
+4號餐:ice cream,65""")
+
+
+isok = False
+while isok == False:
+        need=int(input("need?>"))
+        if need < 1 or need > 4:
+            print("1~4號餐")
+        else:
+            if need == 1:
+                print(a)
+            elif need == 2:
+                print(b)
+            elif need == 3:
+                print(c)
+            else:
+                print(d)
+            isok == True
+            break
