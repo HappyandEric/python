1ee08ee7ee41f3942ca79a5cd196c2616d358bda
HappyandEric <88917824+HappyandEric@users.noreply.github.com>
2021-11-05 21:14:08 +0800

    Update play.py

--- play.py
+++ play.py
@@ -1,25 +1,36 @@
-a=["吐司,50元"]
-b=["布丁,15元"]
-c=["apple,40元"]
-d=["ice cream,65元"]
-print("""1號餐:吐司,40元
-2號餐:布丁,15元
-3號餐:apple,40元
-4號餐:ice cream,65元""")
+# coding:utf-8
 
-isok = False
-while isok == False:
-        need=int(input("need?>"))
-        if need < 1 or need > 4:
-            print("1~4號餐")
+print("歡迎來到英文字典")
+while True:
+    print("""1.目錄
+2.英文字典
+3.學習成果測驗(非常難，老師你應該不行[我建議貴老師您不要嘗試])
+4.列出單字
+5.離開系統""")
+    abc=int(input(">"))
+    if abc == 1:
+        print("a~z")
+    elif abc == 2:
+        dictionary={
+        'a':'(airport,機場),(airplant,飛機),(away,永遠),(Africa,非洲),(African,非洲的\人)',
+        'b':'(bomb,炸彈),(biography,傳(念ㄓㄨㄢˋ)),(beat,打敗),(bee,蜜蜂),(battery,電池)'}
+        a=input("(a~z)?>")
+        n=dictionary.get(a)
+        print(n)
+    elif abc == 3:
+        apron=input("apron=?>")
+        if apron == ("停機坪"):
+            print("yes,good job")
         else:
-            if need == 1:
-                print(a)
-            elif need == 2:
-                print(b)
-            elif need == 3:
-                print(c)
-            else:
-                print(d)
-            isok == True
-            break
+            print("no,answer:停機坪")
+    elif abc == 4:
+        for i in dictionary.keys():
+            print(i)
+    
+        for i in dictionary.values():
+            print(i)
+    
+        for i in dictionary.items():
+            print(i)
+    elif abc == 5:
+        break
